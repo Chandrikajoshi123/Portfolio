b7ba490582f980f3ddaba6e4e8e0cdcfa2f092e9
chandrika joshi <100508364+Chandrikajoshi123@users.noreply.github.com>
2024-07-23 12:05:55 +0530

    Add files via upload

---
+++ Myapp.py
@@ -0,0 +1,60 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+st.write(" ")
+st.subheader("Hello 👋🏻") 
+st.title("My Portfolio Website")
+st.write("""~ ✨ Enthusiastic student with a passion for Python programming, Power BI analytics, and machine learning applications. Actively seeking new opportunities to apply and enhance my skills in a dynamic professional environment. Eager to contribute to innovative projects and learn from experienced professionals in the field.✨ ~""")
+st.write("-------")
+st.subheader("Connect with Me")
+st.write("[GitHub](https://github.com/Chandrikajoshi123) | [LinkedIn](https://www.linkedin.com/in/chandrika-j-0b1a98238?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app)")
+
+st.write("-------")
+st.subheader("About Me  ")
+st.write("""Hello! I’m Chandrika Joshi, a passionate data scientist with a unique blend of scientific curiosity and analytical expertise. My academic journey began at the Modern Institute of Technology, where I explored the intricacies of Physics, Chemistry, and Mathematics, laying a robust foundation for my analytical skills. 🔬✨\n
+Currently, I’m advancing my knowledge with a Master’s in Data Science and Statistics at Graphic Era Hill University in Dehradun. 📊🎓 My studies are not just expanding my technical skills but also fueling my enthusiasm for harnessing data to drive impactful insights and innovations.\n
+Here on my portfolio, you’ll find a showcase of my journey, projects, and the dynamic intersections where data science meets creativity. 🌟🔍 I am excited to connect with fellow professionals and explore new opportunities in this ever-evolving field.""")
+st.write("________")
+st.subheader("Projects")
+st.write("[AniGen](https://github.com/Chandrikajoshi123/PartyRock)\n An innovative app created for a hackathon using PartyRock AWS. AniGen allows users to enter a character's name and generates a unique story featuring that character. You can also engage in interactive chats with your character, bringing your storytelling to life.")
+st.write("[AnimeCiencia](https://github.com/Chandrikajoshi123/AnimeCiencia)\n An innovative project tha tmerges anime fandom with scientific exploration.")
+st.write("[mL Project](https://github.com/Chandrikajoshi123/ML-Projects)\n A collection of diverse machine learning projects.")
+st.write("- - - - ")
+st.subheader("Experience")
+st.write("""➡ **Power BI Intern
+SMNG Academy** \n
+January 2024 – March 2024
+During my internship at SMNG Academy, I completed three significant projects:\n
+
+⚬ Sales Data Analysis 📈\n
+⚬ Global Terrorism Analysis 🌍\n
+⚬ Sales Data Analysis 📊""")
+st.write("""➡ **Member** \n
+Google Developer Student Club (GDSC), Graphic Era University \n
+January 2024 – Present
+Actively contributing to various initiatives, enhancing my skills and knowledge through collaborative efforts""")
+st.write("""➡ **AI and Data Science Intern**\n
+Coding Jr\n
+June 2024 – Present
+Gaining hands-on experience in AI and data science, working on real-world projects and expanding my expertise in data analytics and machine learning.""")
+st.write("""➡ **Indigo Squad Member
+Mood Indigo, IIT Bombay** \n
+July 2024 – Present
+Collaborating with a team to manage and execute various projects and events, contributing to the vibrant community of Mood Indigo at IIT Bombay.""")
+st.write("---------")
+st.subheader("Languages")
+st.write("English 🇺🇸")
+st.write("Hindi 🇮🇳")
+st.write("Spanish 🇪🇸")
+st.write("--------")
+st.subheader("Skills")
+st.write("""  ➜  R\n 
+  ➜  DBMS\n 
+  ➜  Microsoft PowerPoint\n
+  ➜  Machine Learning\n 
+  ➜  Python\n 
+  ➜  AI\n 
+  ➜  Data Cleaning\n 
+  ➜  LLM deployment""")
+st.write("----------")
+st.write("Thank you for visiting my portfolio. Feel free to connect with me on [Email](jchandrika67@gmail.com) ")
